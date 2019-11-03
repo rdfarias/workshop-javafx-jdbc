5fddb324f60dc04757a5891ed5aa2de47c2b301f
rdfarias@zipmail.com.br <Adminstrador@Lenovo-PC>
2019-11-03 12:12:21 -0200

    Remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,10 +1,13 @@
 package gui.util;
 
+import java.util.Optional;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class Alerts {
-	
+
 	public static void showAlert(String title, String header, String content, AlertType type) {
 
 		Alert alert = new Alert(type);
@@ -17,6 +20,14 @@ public class Alerts {
 
 		alert.show();
 
+	}
+
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
 	}
 
 }
